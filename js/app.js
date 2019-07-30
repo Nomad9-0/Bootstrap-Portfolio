73c2ae3b7ef4c504c1a74ebdce052221492e7fb2
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-07-29 18:29:55 -0700

    JS added

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navbarNav");
-    if (x.className === "navbar-collapse") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-collapse";
-    }
-}
